2da8d3953acca845040eec641a13e69a39668b54
LewJun <lewjun072@sina.com>
2019-08-20 16:53:25 +0800

    emp/index开发

--- Emp-web/src/main/java/com/example/lewjun/controller/EmpController.java
+++ Emp-web/src/main/java/com/example/lewjun/controller/EmpController.java
@@ -1,9 +1,17 @@
 package com.example.lewjun.controller;
 
+import com.example.lewjun.bean.po.Emp;
 import com.example.lewjun.service.EmpService;
+import com.google.common.collect.Maps;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author LewJun
@@ -14,9 +22,13 @@ public class EmpController extends BaseController {
     @Autowired
     private EmpService empService;
 
-    @RequestMapping("index")
-    public String index() {
-        LOGGER.info("【empService.findAll() {}】", empService.findAll());
-        return "emp/index";
+    @RequestMapping(value = "index", method = RequestMethod.GET)
+    @ResponseBody
+    public ModelAndView index() {
+        Map<String, List<Emp>> modelMap = Maps.newHashMap();
+        List<Emp> empList = empService.findAll();
+        LOGGER.info("【empList:{}】", empList);
+        modelMap.put("empList", empList);
+        return render("emp/index", modelMap);
     }
 }
--- Emp-web/src/main/webapp/WEB-INF/views/emp/index.jsp
+++ Emp-web/src/main/webapp/WEB-INF/views/emp/index.jsp
@@ -1,1 +1,35 @@
-1
+<%@ page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Emp</title>
+<body>
+<h2>Emp manager!</h2>
+<table>
+  <tbody>
+  <tr>
+    <th>Empno</th>
+    <th>Ename</th>
+    <th>Opr</th>
+  </tr>
+  <c:if test="${empList != null}">
+    <c:forEach items="${empList}" var="emp">
+      <tr>
+        <td>${emp.empno}</td>
+        <td>${emp.ename}</td>
+        <td>
+          <a href="javascript:void(0)" class="emp-del">Del</a>
+          <a href="javascript:void(0)" class="emp-edit">Edit</a>
+        </td>
+      </tr>
+    </c:forEach>
+  </c:if>
+  </tbody>
+</table>
+</body>
+</html>
