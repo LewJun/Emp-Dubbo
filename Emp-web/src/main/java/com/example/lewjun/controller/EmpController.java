03eb7554e3d1f384321f6c9ab0ead40a394e2a58
LewJun <lewjun072@sina.com>
2019-08-20 17:23:43 +0800

    使用@Reference对要使用的服务名称进行注解配置，方便dubbo:annotation配置的包扫描

--- Emp-web/src/main/java/com/example/lewjun/controller/EmpController.java
+++ Emp-web/src/main/java/com/example/lewjun/controller/EmpController.java
@@ -1,9 +1,9 @@
 package com.example.lewjun.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.example.lewjun.bean.po.Emp;
 import com.example.lewjun.service.EmpService;
 import com.google.common.collect.Maps;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,7 +19,7 @@ import java.util.Map;
 @Controller
 @RequestMapping("emp")
 public class EmpController extends BaseController {
-    @Autowired
+    @Reference
     private EmpService empService;
 
     @RequestMapping(value = "index", method = RequestMethod.GET)
